ea87f53c0d7bc18574fc112f9ac4fc8aa70535ec
Jonatan Linares <jonlinper@gmail.com>
2017-02-20 16:44:18 +0100

    Extcom program

---
+++ source/programs/extcom.c
@@ -0,0 +1,15 @@
+/*
+* Extcom for NANO: External communication through Serial port.
+* By Jonatan Linares.
+*/
+
+#include "types.h"
+#include "ulib/ulib.h"
+
+#define NAME_LEN 50
+
+uint main(uint argc, uchar* argv[]) {
+  sputstr("Wolaaaa!\n\r");
+
+  return 0;
+}
