91d2e0b12d8c5e7aa094290964330d0224a7e981
Jonatan Linares <jonlinper@gmail.com>
2017-02-24 13:02:06 +0100

    Chat program 2.0

--- source/programs/chat.c
+++ source/programs/chat.c
@@ -8,7 +8,7 @@
 #include "ulib/ulib.h"
 
 #define NICK_LEN 50
-#define BUFFER_LEN 10000
+#define BUFFER_LEN 250
 
 uint main(uint argc, uchar* argv[]) {
   uchar* nick = malloc(NICK_LEN + 1);
@@ -20,10 +20,12 @@ uint main(uint argc, uchar* argv[]) {
   uint rKey;
   uint key;
   uint i;
+  uint current_line = 7;
 
   iBuffer[0] = 0;
   oBuffer[0] = 0;
 
+  clear_screen();
   putstr("NANO-Chat\n\r");
   putstr("v0.2\n\r");
   putstr("---------\n\r");
@@ -41,9 +43,11 @@ uint main(uint argc, uchar* argv[]) {
     if (data == 59) {// Entire (Ends with ";")
       if (oBuffer[0] != 0) {
         putstr("\n\r");
+        current_line++;
       }
 
-      putstr("%s\n\r", iBuffer);
+      putstr("--- %s\n\r", iBuffer);
+      current_line++;
       iBuffer[0] = 0;
 
       putstr("%s", oBuffer);
@@ -57,7 +61,7 @@ uint main(uint argc, uchar* argv[]) {
 
     // Send
     
-    for(i = 0; i < 50000; i++) {
+    for(i = 0; i < 32000; i++) {
       rKey = getkey(NO_WAIT);// oBuffer, BUFFER_LEN
       key = getLO(rKey);
 
@@ -69,9 +73,21 @@ uint main(uint argc, uchar* argv[]) {
       else if (key == KEY_LO_RETURN) {// Entire ("Enter" pressed)
         if (oBuffer[0] != 0) {
           putstr("\n\r");
+          current_line++;
 
           sputstr("%s: %s;", nick, oBuffer);
           oBuffer[0] = 0;
+        }
+      }
+      else if (key == KEY_LO_BACKSPACE) {// Backspace
+        len = strlen(oBuffer);
+
+        if (len > 0) {
+          set_cursor_position(len - 1, current_line);
+          putchar(32);// Space
+          set_cursor_position(len - 1, current_line);
+
+          oBuffer[len - 1] = 0;
         }
       }
       else if (key != 0) {// Fragment
